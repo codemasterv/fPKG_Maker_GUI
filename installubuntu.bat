4223f31d92841409ef3b70cfd5c67728d3da971d
codemasterv <codemasterv@gmail.com>
2021-12-31 16:27:41 -0600

    First Release!
    Please report bugs and errors and I will address them as time permits

---
+++ installubuntu.bat
@@ -0,0 +1,5 @@
+@ECHO off
+install -d Ubuntu-20.04
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install dos2unix -y 
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install wget -y
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install curl -y
---
+++ yourftpdump.bat
@@ -0,0 +1,3 @@
+@ECHO off
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e dos2unix yourftpdump
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e ./yourftpdump %*
