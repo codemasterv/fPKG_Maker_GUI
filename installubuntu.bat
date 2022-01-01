eb4f6504af09da64cfc98a4b5f87de584063d195
Robert Pendell <shinji257@users.noreply.github.com>
2021-12-31 22:07:52 -0500

    Update installubuntu.bat
    Making the same change here for the same reason

--- installubuntu.bat
+++ installubuntu.bat
@@ -1,5 +1,5 @@
 @ECHO off
 wsl --install -d Ubuntu-20.04
-%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install dos2unix -y 
-%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install wget -y
-%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install curl -y
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install dos2unix -y 
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install wget -y
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install curl -y
