815dfc25c7a3c854913e8478435b9a743117cda2
codemasterv <codemasterv@gmail.com>
2021-12-31 19:32:59 -0600

    added fix for windows 10
    dump these files in the root directory with the program if you are on windows 10

---
+++ win10ftpdumpfiles/installubuntu.bat
@@ -0,0 +1,4 @@
+wsl --install -d Ubuntu-20.04
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install dos2unix -y 
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install wget -y
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install curl -y
---
+++ win10ftpdumpfiles/yourftpdump.bat
@@ -0,0 +1,2 @@
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e dos2unix yourftpdump
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo ./yourftpdump
