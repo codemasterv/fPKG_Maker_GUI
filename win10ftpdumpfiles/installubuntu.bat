896a653203cf307bcdd75508aa3736c1e94d9bf5
Robert Pendell <shinji257@users.noreply.github.com>
2021-12-31 22:09:11 -0500

    Update installubuntu.bat

--- win10ftpdumpfiles/installubuntu.bat
+++ win10ftpdumpfiles/installubuntu.bat
@@ -1,4 +1,4 @@
 wsl --install -d Ubuntu-20.04
-%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install dos2unix -y 
-%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install wget -y
-%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install curl -y
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install dos2unix -y 
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install wget -y
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install curl -y
