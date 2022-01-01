3c8a39ca5fa3871a93a84c9876029918cc74dd89
codemasterv <codemasterv@gmail.com>
2022-01-01 00:43:06 -0600

    revert
    revert

--- yourftpdump.bat
+++ yourftpdump.bat
@@ -1,3 +1,3 @@
 @ECHO off
-%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e dos2unix yourftpdump
-%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e ./yourftpdump %*
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e dos2unix yourftpdump
+%HOMEPATH%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e ./yourftpdump %*
