8abc6c317b542833ea0a8d32b60c078ffe16d0b3
codemasterv <codemasterv@gmail.com>
2022-01-03 15:57:34 -0600

    updated ubuntu.bat and youftpdump.bat
    updated scripts to run updates and to also start a fresh wsl session for dumps.
    
    YOU CAN NOT DUMP TO A USB DRIVE DO TO WSL LIMITS

--- installubuntu.bat
+++ installubuntu.bat
@@ -1,5 +1,9 @@
 @ECHO off
 wsl --install -d Ubuntu-20.04
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt-get update -y
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt-get upgrade -y
 %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install dos2unix -y 
 %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install wget -y
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install curl -y
+
 
