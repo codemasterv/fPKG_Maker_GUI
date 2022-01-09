76be0756986d3c056eae0747adc669ecfb1807d5
codemasterv <codemasterv@gmail.com>
2022-01-08 22:31:16 -0600

    Updated UI to include New Features
    Bug fixed where it continued to run in the background even after closing
    
    Added the ability to just dump,
    
    Base game
    
    Patch
    
    Keystone
    
    DLC
    
    Added simple start sound "Number Thumper" from tetris effect

--- installubuntu.bat
+++ installubuntu.bat
@@ -1,9 +1,5 @@
 @ECHO off
-wsl --install -d Ubuntu-20.04
-%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt-get update -y
-%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt-get upgrade -y
+install -d Ubuntu-20.04
 %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install dos2unix -y 
 %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install wget -y
 %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e sudo apt install curl -y
-
-
--- patchftpdump.bat
+++ patchftpdump.bat
@@ -1,4 +1,4 @@
 @ECHO off
 %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e --shutdown
 %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e dos2unix yourftpdump
-%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e ./yourftpdump %*
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe run -e ./yourftpdump -p %*
